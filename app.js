895511a0a7c90522aed3a9e1e9fbfca97aaf0e6e
aldrit <aldritkuleta2@gmail.com>
2024-01-30 14:23:27 +0100

    fonction a moitié finit pour timer sans bug

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+window.addEventListener("load", (event) => {
+    console.log("La page est complètement chargée");
+ 
 /***************** Intro *****************/
 //Ciblage des élements du DOM
 // let mainTitle = document.querySelector("h1")
@@ -50,7 +53,64 @@ let timeout;
 
 let estArrete = true;
 
-function demarrer
+const demarrer = () => {
+    if(estArrete) {
+        estArrete = false;
+        defilerTemps();
+    }
+}
+
+const arreter =  () => {
+    if(estArrete = true);
+    clearTimeout(timeout);
+}
+
+const defilerTemps = () => {
+    if(estArrete) return;
+    
+    secondes = parseInt(secondes);
+    minutes = parseInt(minutes);
+    heures = parseInt(heures);
+
+    secondes++;
+
+    if(secondes == 60) {
+        minutes++;
+        secondes = 0;
+    }
+
+    if(minutes == 60) {
+        heures++;
+        minutes = 0;
+    }
+
+    //affichage
+    if(secondes < 10) {
+        secondes = "0" + secondes;
+    }
+
+    if(minutes < 10) {
+        minutes = "0" + minutes;
+    }
+
+    if(heures < 10) {
+        heures = "0" + heures;
+    }
+    
+    Timer.textContent(`${heures}:${minutes}:${secondes}`);
+
+    timeout = setTimeout(defilerTemps, 1000);
+};
+
+const reset = () => {
+    chrono.textContent = "00:00:00";
+    estArrete = true;
+    heures = 0;
+    minutes = 0;
+    secondes = 0;
+    clearTimeout(timeout);
+}
+
 
 /*********************** Timer ***************************/
 //creation de la box en div
@@ -95,3 +155,9 @@ for(let i = 1; i <=valuePrompt; i++) {
 }
 
 shuffleChildren(board) //on mélange les enfants du parent board
+
+
+
+
+
+});
